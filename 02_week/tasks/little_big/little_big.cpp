1ff35d1f04cc9fcbc7e1dc5eae186cce99e2e9ee
Dsv9toy <danilka6932@mail.ru>
2025-12-08 18:43:12 +0500

    try fix little_big

--- 02_week/tasks/little_big/little_big.cpp
+++ 02_week/tasks/little_big/little_big.cpp
@@ -9,6 +9,8 @@
 В функцию передается вторым аргументом флаг который по запросу перевернет представление числа. Слева направо или справа налево (BE или LE). По умолчанию не инвертируем
 // Для преобразования цифры в число сделаем массив всех чисел и букв для 16-ричной системы счисления
 // Затем сделаем цифры символами чтобы вывести их на экран
+// Тесты прошли, однако настроенный CI в гитхабе заругался на попытку сравнения знакового и беззнакового типа в цикле
+
 */
 void PrintMemory(int number, bool reverseFlag = false) 
 {
@@ -16,16 +18,17 @@ void PrintMemory(int number, bool reverseFlag = false)
     const char hexChars[] = "0123456789ABCDEF";
     
     std::cout << "0x";
+    const int size = static_cast<int>(sizeof(number));
     
     if (!reverseFlag) {
-        for (int i = 0; i < sizeof(number); ++i) {  
+        for (int i = 0; i < size; ++i) {  
             unsigned char currentByte = bytes[i];
             std::cout << hexChars[currentByte >> 4];
             std::cout << hexChars[currentByte & 0x0F];
         }
     } else {
         // С реверсом (BE)
-        for (int i = sizeof(number) - 1; i >= 0; --i) {  
+        for (int i = size - 1; i >= 0; --i) {  
             unsigned char currentByte = bytes[i];
             std::cout << hexChars[currentByte >> 4];
             std::cout << hexChars[currentByte & 0x0F];
@@ -41,16 +44,16 @@ void PrintMemory(double number, bool reverseFlag = false)
     const char hexChars[] = "0123456789ABCDEF";
     
     std::cout << "0x";
-    const int total_bytes = sizeof(double);
+    const int size = static_cast<int>(sizeof(number));
     
     if (!reverseFlag) {
-        for (int i = 0; i < total_bytes; ++i) {  
+        for (int i = 0; i < size; ++i) {  
             unsigned char currentByte = bytes[i];
             std::cout << hexChars[currentByte >> 4];
             std::cout << hexChars[currentByte & 0x0F];
         }
     } else {
-        for (int i = total_bytes - 1; i >= 0; --i) {  
+        for (int i = size - 1; i >= 0; --i) {  
             unsigned char currentByte = bytes[i];
             std::cout << hexChars[currentByte >> 4];
             std::cout << hexChars[currentByte & 0x0F];
